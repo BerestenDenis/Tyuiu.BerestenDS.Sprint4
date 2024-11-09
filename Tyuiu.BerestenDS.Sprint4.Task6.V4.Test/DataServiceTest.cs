b7de18b7b8210572ce3bd0597bd0aa9c89cbeb22
Денис Берестень <d.blyl1337@gmail.com>
2024-11-09 18:29:07 +0500

    Task6

---
+++ Tyuiu.BerestenDS.Sprint4.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+using Tyuiu.BerestenDS.Sprint4.Task6.V4.Lib;
+namespace Tyuiu.BerestenDS.Sprint4.Task6.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var array = new string[] { "Москва", "Санкт Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казахстан", "Самара" };
+            string[] res = ds.Calculate(array);
+            string[] wait = { "Москва", "Самара" };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
